70368d95f60c0efa0eccfff9af65cc15a823e3ea
zenepvarol <128228755+zenepvarol@users.noreply.github.com>
2026-04-01 16:50:50 +0300

    chore: integrate Vuetify 3 framework into the project

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 // Vue uygulamasının başlatıldığı, global eklentilerin ve stil dosyalarının sisteme dahil edildiği ana merkez
 
 import './assets/main.css' // Uygulamanın temel CSS yapılandırması
+import 'vuetify/styles' // Vuetify'ın temel stilleri
 
 import { createApp } from 'vue' // Vue uygulama örneğini oluşturmak için gerekli metod
 import { createPinia } from 'pinia' // Merkezi veri yönetimi (Store) için Pinia
+import { createVuetify } from 'vuetify' // Vuetify eklentisinitanımlamak için
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import App from './App.vue' // Kök bileşen (Root Component)
 import router from './router' // Sayfa yönlendirmelerini (Routing) yöneten yapı
@@ -11,10 +15,16 @@ import router from './router' // Sayfa yönlendirmelerini (Routing) yöneten yap
 // Görsel Efektler ve İkon Setleri
 import '@mdi/font/css/materialdesignicons.css'; // MDI ikon seti
 
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 const app = createApp(App)
 
-app.use(createPinia()) // Vue Uygulama Örneği (Instance) Tanımlama
-app.use(router) // Global State Management (Pinia) Entegrasyonu
+app.use(createPinia()) // Global State Management (Pinia) Entegrasyonu
+app.use(router) // Router Entegrasyonu
+app.use(vuetify) // Vuetify Entegrasyonu
 
 // Hazırlanan Uygulama Yapısının DOM Üzerine Render Edilmesi (Mounting)
 app.mount('#app')
